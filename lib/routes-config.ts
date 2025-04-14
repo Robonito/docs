735d8928284bef383273580195397ae7a27c6472
HumaSofy <huma@sofyrus.com>
2025-04-14 17:05:21 +0530

    docs structure changes

--- lib/routes-config.ts
+++ lib/routes-config.ts
@@ -9,25 +9,113 @@ export type EachRoute = {
 
 export const ROUTES: EachRoute[] = [
   {
-    title: "Introduction",
-    href: "/introduction"
+    title: "Quick Tour",
+    href: "/quick-tour",
   },
   {
     title: "Installation",
-    href: "/installation"
-  },
-  {
-    title: "Quick Tour",
-    href: "/quick-tour"
-  },
-  {
-    title: "Project Organization",
-    href: "/project-organization",
+    href: "/installation",
     items: [
-      { title: "Workspace", href: "/workspace" },
-      { title: "Suites", href: "/suites" },
-      { title: "Test Cases", href: "/test-cases" }     
+      { title: "Mac", href: "/installation/mac" },
+      { title: "Windows", href: "/installation/window" },
+      { title: "Linux", href: "/installation/linux" },
     ],
+  },
+  {
+    title: "Workspaces",
+    href: "/workspaces",
+  },
+  {
+    title: "Suites",
+    href: "/suites",
+  },
+  {
+    title: "Test Case",
+    href: "/test-case",
+  },
+  {
+    title: "Web Testing",
+    href: "/web-testing",
+    items: [
+      {
+        title: "Record Test case",
+        href: "/web-testing/record test-case",
+        items: [
+          {
+            title: "Record & playback",
+            href: "/web-testing/record test-case/record-playback",
+          },
+          {
+            title: "AI recording",
+            href: "/web-testing/record test-case/ai-recording",
+          },
+        ],
+      },
+
+      {
+        title: "Assertions",
+        href: "/web-testing/assertions",
+        items: [
+          { title: "AI Generated", href: "/ai-generated" },
+          { title: "Manual", href: "/manual" },
+        ],
+      },
+      {
+        title: "Recording variables",
+        href: "/web-testing/recording-variables",
+      },
+      {
+        title: "Executions",
+        href: "/web-testing/executions",
+        items: [
+          { title: "Cloud", href: "/cloud" },
+          { title: "Local", href: "/local" },
+        ],
+      },
+    ],
+  },
+
+  {
+    title: "API Testing",
+    href: "/api-testing",
+    items: [
+      { title: "1", href: "/1" },
+      { title: "2", href: "/2" },
+      { title: "3", href: "/3" },
+    ],
+  },
+  {
+    title: "Executions",
+    href: "/executions",
+    items: [
+      { title: "Cloud", href: "/cloud" },
+      { title: "Local", href: "/local" },
+    ],
+  },
+  {
+    title: "Variables",
+    href: "/variables",
+  },
+  {
+    title: "Teams",
+    href: "/teams",
+  },
+  {
+    title: "Integrations",
+    href: "/integrations",
+    items: [
+      { title: "Email", href: "/email" },
+      { title: "Jira", href: "/jira" },
+      { title: "Github", href: "/github" },
+    ],
+  },
+  {
+    title: "Reports",
+    href: "/reports",
+  },
+  {
+    title: "Schedules",
+    href: "/schedules",
   },
 ];
 
